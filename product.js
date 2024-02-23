1aa6fc71cb2c428a8c3f27347109255863f26926
GreenBoy818 <mohd.ali2002@gmail.com>
2024-02-23 15:06:02 +0400

    Pushing files

---
+++ product.js
@@ -0,0 +1,10 @@
+let product = [
+{
+    id: 1001,
+    title: "Cat Food, 25lb bag",
+    description: "A 25 pound bag of <em>irresistible</em>," + "organic goodness for your cat",
+    price: 2000,
+    image: "images/Pinkcat.png"
+}
+]
+
